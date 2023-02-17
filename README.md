48dbe89d6bf17855601f9e240a1ee6ec3ddecb8a
supratim531 <91423964+supratim531@users.noreply.github.com>
2023-02-17 22:34:14 +0530

    Merge last update with netlify

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Getting Started with Create React App
 
-## Here is our [Live-App](https://wormbook.netlify.app)
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ src/component/content/Content.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Content() {
+  return (
+    <>
+      <div className="my-4 shadow rounded bg-[#ffd8e5]">
+        <div className="py-2 flex flex-wrap gap-2 justify-center items-center">
+          <div className="w-[120px] h-[120px] xs:w-[150px] xs:h-[150px] ms:w-[230px] ms:h-[230px] rounded bg-white"></div>
+          <div className="w-[120px] h-[120px] xs:w-[150px] xs:h-[150px] ms:w-[230px] ms:h-[230px] rounded bg-white"></div>
+          <div className="w-[120px] h-[120px] xs:w-[150px] xs:h-[150px] ms:w-[230px] ms:h-[230px] rounded bg-white"></div>
+          <div className="w-[120px] h-[120px] xs:w-[150px] xs:h-[150px] ms:w-[230px] ms:h-[230px] rounded bg-white"></div>
+          <div className="w-[120px] h-[120px] xs:w-[150px] xs:h-[150px] ms:w-[230px] ms:h-[230px] rounded bg-white"></div>
+          <div className="w-[120px] h-[120px] xs:w-[150px] xs:h-[150px] ms:w-[230px] ms:h-[230px] rounded bg-white"></div>
+          <div className="w-[120px] h-[120px] xs:w-[150px] xs:h-[150px] ms:w-[230px] ms:h-[230px] rounded bg-white"></div>
+          <div className="w-[120px] h-[120px] xs:w-[150px] xs:h-[150px] ms:w-[230px] ms:h-[230px] rounded bg-white"></div>
+          <div className="w-[120px] h-[120px] xs:w-[150px] xs:h-[150px] ms:w-[230px] ms:h-[230px] rounded bg-white"></div>
+          <div className="w-[120px] h-[120px] xs:w-[150px] xs:h-[150px] ms:w-[230px] ms:h-[230px] rounded bg-white"></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Content;
